dbd46f8c18055b3f12237fc6962de5aab23da580
Bence Monus <crome@moronia.hu>
2017-09-14 23:24:01 +0100

    insert full datetime

--- sql/mailboxes.sql
+++ sql/mailboxes.sql
@@ -2,7 +2,7 @@ CREATE TABLE mailboxes (
   address TEXT NOT NULL,
   password TEXT NOT NULL,
   maildir TEXT NOT NULL,
-  created TEXT DEFAULT CURRENT_DATE,
+  created TEXT DEFAULT CURRENT_TIMESTAMP,
   uid INT NOT NULL,
   gid INT NOT NULL,
   PRIMARY KEY (address)
