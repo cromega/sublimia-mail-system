74188a81cc7bead51d0dc994a504811c71e8c1c3
Bence Monus <crome@moronia.hu>
2017-09-18 21:31:29 +0100

    fix mail db path

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL maintainer="crome@moronia.hu"
 
 ENV DEBIAN_FRONTEND noninteractive
 ENV INSTALL_PACKAGES "bash vim postfix postfix-sqlite sasl2-bin libsasl2-modules courier-imap courier-authlib-sqlite sqlite3 rsyslog ca-certificates"
-ENV MAIL_DB_PATH /etc/postfix/mail.db
+ENV MAIL_DB_PATH /etc/postfix/mail.db.sqlite
 
 RUN apt-get update && apt-get -y install --no-install-recommends $INSTALL_PACKAGES
 RUN groupadd -g 5000 vmail && \
--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ echo 127.0.0.1 test.sublimia.nl >> /etc/hosts
 echo quit | openssl s_client -connect localhost:587 -starttls smtp 2>&1 | grep "Verify return code: 0 (ok)"
 
 # SMTP AUTH
-sqlite3 /etc/postfix/mail.db 'INSERT INTO mailboxes VALUES ("test@test.com", "{SHA256}n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg=", "test.com/test", CURRENT_TIMESTAMP, 5000, 5000);'
+sqlite3 "$MAIL_DB_PATH" 'INSERT INTO mailboxes VALUES ("test@test.com", "{SHA256}n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg=", "test.com/test", CURRENT_TIMESTAMP, 5000, 5000);'
 
 (echo -e "ehlo test@test.com\r\nAUTH PLAIN dGVzdEB0ZXN0LmNvbQB0ZXN0QHRlc3QuY29tAHRlc3Q="; sleep 1) | openssl s_client -connect localhost:587 -starttls smtp 2>&1 | grep "235 2.7.0 Authentication successful"
 
